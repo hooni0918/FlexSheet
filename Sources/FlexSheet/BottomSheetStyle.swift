f02ef44d2eb144857bdef8b9762de0071cb47d63
hooni <jhlee909090@gmail.com>
2025-02-09 03:38:03 +0900

    Update BottomSheetStyle.swift for improved layout calculations
    - Added constants for itemHeight and headerHeight
    - Adjusted height calculation for .half and .minimal cases

--- Sources/FlexSheet/BottomSheetStyle.swift
+++ Sources/FlexSheet/BottomSheetStyle.swift
@@ -15,13 +15,16 @@ public enum BottomSheetStyle: Equatable {
     case notShow
     
     public func height(for screenHeight: CGFloat) -> CGFloat {
+        let itemHeight: CGFloat = 120
+        let headerHeight: CGFloat = 60
+        
         switch self {
         case .full:
             return screenHeight * 0.876
         case .half:
-            return screenHeight * 0.5
+            return (itemHeight * 2) + headerHeight // 2개 아이템 + 헤더
         case .minimal:
-            return screenHeight * 0.25
+            return headerHeight // 헤더만
         case .notShow:
             return 0
         }
